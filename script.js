dc9b77077430fd0cc164fa758d3ffa99f59d30eb
jeremytfreeman <jfree7@gmail.com>
2024-08-25 16:01:08 -0400

    ypdate about section content

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 
 //back to top button functions 
+
 //display button when scrolled
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
       mybutton.style.display = "block";
     } else {
+      //else hide button
       mybutton.style.display = "none";
     }
   }
 
   //scroll page back to top
   function topFunction() {
-    document.body.scrollTop = 0;
+    //document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
   }
+
 
 // Get the back to top button
 let mybutton = document.getElementById("topBtn");
